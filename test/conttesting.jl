786d9ae6779f920069bee0dd6f3e8a29bbcec506
Waiss Azizian <waiss.azizian@ens.fr>
2021-12-13 14:36:09 +0100

    Reduce pause in revise.jl

--- test/conttesting.jl
+++ test/conttesting.jl
@@ -3,7 +3,7 @@ using OnlineSampling
 
 testdir = dirname(@__FILE__)
 
-entr([testdir], [OnlineSampling]; postpone=false) do
+entr([testdir], [OnlineSampling]; postpone=false, pause=0.001) do
     Base.run(`clear`)
     try
         include(joinpath(testdir, "runtests.jl"))
