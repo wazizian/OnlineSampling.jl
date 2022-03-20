bffe4282256866108792525c871f2903dd36a1b2
Waiss Azizian <wazizian@clipper.ens.fr>
2022-03-20 20:32:30 +0100

    Uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,33 +25,33 @@ include("custom_testset.jl")
 include(joinpath(testdir, "online_smc/utils.jl"))
 
 @testset TS exit_on_error = exit_on_error "OnlineSampling.jl" begin
-    #     @testset "cd gaussian" begin
-    #         include(joinpath(testdir, "delayed_sampling/cd_gaussian.jl"))
-    #     end
-    #     @testset "simple gaussian" begin
-    #         include(joinpath(testdir, "delayed_sampling/simple_gaussian.jl"))
-    #     end
-    #     @testset "tree of gaussians" begin
-    #         include(joinpath(testdir, "delayed_sampling/tree_gaussian.jl"))
-    #     end
+    @testset "cd gaussian" begin
+        include(joinpath(testdir, "delayed_sampling/cd_gaussian.jl"))
+    end
+    @testset "simple gaussian" begin
+        include(joinpath(testdir, "delayed_sampling/simple_gaussian.jl"))
+    end
+    @testset "tree of gaussians" begin
+        include(joinpath(testdir, "delayed_sampling/tree_gaussian.jl"))
+    end
     @testset "online smc" begin
         include(joinpath(testdir, "online_smc/simple_gaussian.jl"))
     end
-    #     @testset "notinit" begin
-    #         include(joinpath(testdir, "notinit.jl"))
-    #     end
-    #     @testset "notinit propagation" begin
-    #         include(joinpath(testdir, "notinit_propagation.jl"))
-    #     end
-    #     @testset "synchronous constructs" begin
-    #         include(joinpath(testdir, "simple_synchronous.jl"))
-    #     end
-    #     @testset "observe" begin
-    #         include(joinpath(testdir, "observe.jl"))
-    #     end
-    #     @testset "node smc" begin
-    #         include(joinpath(testdir, "node_smc.jl"))
-    #     end
+    @testset "notinit" begin
+        include(joinpath(testdir, "notinit.jl"))
+    end
+    @testset "notinit propagation" begin
+        include(joinpath(testdir, "notinit_propagation.jl"))
+    end
+    @testset "synchronous constructs" begin
+        include(joinpath(testdir, "simple_synchronous.jl"))
+    end
+    @testset "observe" begin
+        include(joinpath(testdir, "observe.jl"))
+    end
+    @testset "node smc" begin
+        include(joinpath(testdir, "node_smc.jl"))
+    end
     @testset "node ds" begin
         include(joinpath(testdir, "node_ds.jl"))
     end
